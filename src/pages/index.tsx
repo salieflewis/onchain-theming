4880cbc441257f22411fcd299ea836503c8f26ef
Salief Lewis <salieflewis@gmail.com>
2023-01-25 10:34:14 -0500

    return json stringified stylesheet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-import { Navigation, Hero } from '../components';
+import { Navigation, Hero, Storage } from '../components';
 
 function Page() {
   return (
     <>
       <Navigation />
       <Hero />
+      <br></br>
+      <Storage />
     </>
   );
 }
