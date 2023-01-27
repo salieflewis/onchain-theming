3a878df9701f2e685469376bbdc670b38134607c
Salief Lewis <salieflewis@gmail.com>
2023-01-27 10:04:00 -0500

    approach feels janky, but styling is being pulled via ipfs

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,35 +2,31 @@ import { ConnectKitButton } from 'connectkit';
 import { useAccount } from 'wagmi';
 import { useWeb3Storage } from '../hooks';
 import { Account } from '../components';
+import * as React from 'react';
 
 export function Navigation() {
   const { isConnected } = useAccount();
 
-  const cid = 'bafybeicdumhupgrxwjp2a5ln6t7qhqhfgwbuy7mrikxoyaves2dztao5j4'
+  const [primaryColor, setPrimaryColor] = React.useState<string | null>(null);
+
+  const cid = 'bafybeicdumhupgrxwjp2a5ln6t7qhqhfgwbuy7mrikxoyaves2dztao5j4';
 
   const { unpackedMetadata } = useWeb3Storage(cid);
 
-  // let stringMetadata = unpackedMetadata as string
+  console.log('Unpacked metadata:', unpackedMetadata);
 
-  // console.log(JSON.parse(stringMetadata))
+  React.useEffect(() => {
+    if (unpackedMetadata) {
+      const parsedMetadata = JSON.parse(unpackedMetadata);
+      console.log('Parsed metadata:', parsedMetadata);
+      setPrimaryColor(parsedMetadata.colors.background);
+    }
+  }, [unpackedMetadata]);
 
-  // const parsedMetadata = JSON.parse(unpackedMetadata as string)
-
-  // console.log(parsedMetadata[0])
-
-  const randomHex = '#784569'
-
-  document.documentElement.style.setProperty(
-    '--primary-color',
-    randomHex
-  );
+  document.documentElement.style.setProperty('--primary-color', primaryColor);
 
   return (
-    <div
-      // id='theming-test__header--component'
-      // bg-[var():--primary-color]
-      className='theming-test__header--component sticky top-0 flex w-full justify-between p-6'
-    >
+    <div className='theming-test__header--component sticky top-0 flex w-full justify-between p-6'>
       <div></div>
       <div>
         <ConnectKitButton />
