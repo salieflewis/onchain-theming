805d20bcca990858f907480a70841e2cb95a391a
Salief Lewis <salieflewis@gmail.com>
2023-02-09 17:29:39 -0500

    add buggy write

--- src/components/SaveChanges.tsx
+++ src/components/SaveChanges.tsx
@@ -1,12 +1,28 @@
+import * as React from 'react';
 import { NFTStorage } from 'nft.storage';
 import { useThemeContext } from '../context/ThemeProvider';
+import { usePrepareContractWrite, useContractWrite } from 'wagmi';
+import { platformThemeRegistryAbi } from '../abi';
+import { BigNumber } from 'ethers';
 
 const client = new NFTStorage({
   token: process.env.NEXT_PUBLIC_NFT_STORAGE_KEY as string,
 });
+/**
+ * Read the registry contract defined as an environment variable
+ */
+const themeRegistry = process.env
+  .NEXT_PUBLIC_REGISTRY_CONTRACT as `0x${string}`;
+/**
+ * Grab the platform index defined as an environment variable
+ */
+// @ts-ignore
+const platformIndex = process.env.NEXT_PUBLIC_PLATFORM_INDEX as number;
 
 export function SaveChanges() {
   const { newMetadata } = useThemeContext();
+
+  const [content, setContent] = React.useState<string>('');
 
   async function handleClick() {
     try {
@@ -16,10 +32,24 @@ export function SaveChanges() {
       const blobThemeData = new Blob([newMetadata]);
       const cid = await client.storeBlob(blobThemeData);
       console.log('Theme data stored at:', cid);
+      /**
+       * Set state variable to cid
+       */
+      setContent(cid);
+      setTheme?.();
     } catch (err) {
       console.error(err);
     }
   }
+
+  const { config, error } = usePrepareContractWrite({
+    address: themeRegistry,
+    abi: platformThemeRegistryAbi,
+    functionName: 'setPlatformTheme',
+    args: [BigNumber.from(platformIndex), content],
+  });
+
+  const { write: setTheme } = useContractWrite(config);
 
   return (
     <div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,12 @@ import { Navigation, Hero } from '../components';
 import { ThemeProvider } from '../context/ThemeProvider';
 
 function Page() {
+  // @ts-ignore
+  const platformIndex = process.env.NEXT_PUBLIC_PLATFORM_INDEX as number;
+  
   return (
     <div>
-      <ThemeProvider platformIndex={1}>
+      <ThemeProvider platformIndex={platformIndex}>
         <Navigation />
         <Hero />
       </ThemeProvider>
