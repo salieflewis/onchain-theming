44ff7b2625245e4d5d457d9fcfc00794cd78edac
Salief Lewis <salieflewis@gmail.com>
2023-02-14 09:29:19 -0500

    update onchain theming button

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,9 @@ export function Navigation() {
   return (
     <div className='theming-test__navigation sticky top-0 flex w-full justify-between items-center p-6'>
       <div className='flex gap-x-4'>
-        <span className='text-xl'>Onchain Theming</span>
+        <button className='bg-white text-black px-8 py-2 rounded-3xl hover:bg-[#E1E1E1]'>
+          <span className='text-xl'>Onchain theming</span>
+        </button>
       </div>
       <div className='flex space-x-6'>
         <ConnectKitButton />
