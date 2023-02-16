0e48dba51258704988d9d6cf2a4f7c82ed4eb917
Salief Lewis <salieflewis@gmail.com>
2023-02-16 17:02:18 -0500

    remove test text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 export function Hero() {
   return (
     <div className='container min-w-full min-h-screen flex flex-col'>
-      <div className='theming-test mx-auto my-auto'>Test text</div>
+      <div className='theming-test mx-auto my-auto'></div>
     </div>
   );
 }
